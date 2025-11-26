b8a9f00260847abd721eb4d25a1ec05826ff4279
NirWolfer <nwolfer@nvidia.com>
2025-11-26 11:22:46 +0200

    debug
    Signed-off-by: NirWolfer <nwolfer@nvidia.com>

--- contrib/jenkins_tests/vg.sh
+++ contrib/jenkins_tests/vg.sh
@@ -144,9 +144,19 @@ for test_link in $test_ip_list; do
 			sleep 2
 		done
 
-		eval "${sudo_cmd} $timeout_exe_short env $test_lib_env \
+		eval "${sudo_cmd} timeout -s SIGINT 1m env $test_lib_env \
 			${vg_tool} --log-file=${vg_dir}/${test_name}-valgrind-cl.log $vg_args \
 			$test_app_path ${test_app_client_params} ${test_opt} -i ${test_ip} -t 10 ${test_params} 2>&1 | tee ${vg_dir}/${test_name}-output-cl.log"
+		
+		echo "timeout return code: $?"
+		echo " ----------------------- CHECKING SOCKPERF PROCESSES -----------------------"
+		ps -ef | grep $test_app
+		echo "sockperf client log:"
+		cat "${vg_dir}/${test_name}-output-cl.log"
+		echo "valgrind client log:"
+		cat "${vg_dir}/${test_name}-valgrind-cl.log"
+		echo " ----------------------- CHECKING SOCKPERF PROCESSES DONE -----------------------"
+
 
 		if [ `ps -ef | grep $test_app | wc -l` -gt 1 ];
 		then
